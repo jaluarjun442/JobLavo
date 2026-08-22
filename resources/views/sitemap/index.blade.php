7724290d13a23e0fbdf4299a93a4d5a7e91b691a
jaluarjun442 <160036840+jaluarjun442@users.noreply.github.com>
2026-08-23 03:16:18 +0530

    sitemap change

--- resources/views/sitemap/index.blade.php
+++ resources/views/sitemap/index.blade.php
@@ -4,10 +4,6 @@
 
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 
-    {{-- =====================================================
-         MAIN / STATIC URLS
-    ====================================================== --}}
-
     @foreach($urls as $url)
 
         <url>
@@ -17,43 +13,10 @@
     @endforeach
 
 
-    {{-- =====================================================
-         CATEGORY URLS
-    ====================================================== --}}
-
     @foreach($categories as $category)
 
         <url>
             <loc>{{ url('/category/' . $category->slug) }}</loc>
-        </url>
-
-    @endforeach
-
-</urlset>
-
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-
-    {{-- Static Pages --}}
-
-    @foreach($urls as $url)
-
-        <url>
-
-            <loc>{{ $url }}</loc>
-
-        </url>
-
-    @endforeach
-
-
-    {{-- Categories --}}
-
-    @foreach($categories as $category)
-
-        <url>
-
-            <loc>{{ url('/category/' . $category->slug) }}</loc>
-
         </url>
 
     @endforeach
--- resources/views/sitemap/posts.blade.php
+++ resources/views/sitemap/posts.blade.php
@@ -1,42 +1,11 @@
 @php
     echo '<?xml version="1.0" encoding="UTF-8"?>';
 @endphp
-
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-
-    {{-- =====================================================
-         MAIN / STATIC URLS
-    ====================================================== --}}
-
-    @foreach($urls as $url)
-
-        <url>
-            <loc>{{ $url }}</loc>
-        </url>
-
-    @endforeach
-
-
-    {{-- =====================================================
-         CATEGORY URLS
-    ====================================================== --}}
-
-    @foreach($categories as $category)
-
-        <url>
-            <loc>{{ url('/category/' . $category->slug) }}</loc>
-        </url>
-
-    @endforeach
-
-</urlset>
-
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 
 @foreach($posts as $post)
 
     <url>
-
         <loc>{{ url('/post/' . $post->slug) }}</loc>
 
         @if($post->updated_at)
