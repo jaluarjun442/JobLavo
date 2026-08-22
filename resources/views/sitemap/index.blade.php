962ced2bf4de5de458bbc7ca4ffeb6360b467eda
jaluarjun442 <160036840+jaluarjun442@users.noreply.github.com>
2026-08-23 03:13:33 +0530

    sitmap change

--- resources/views/sitemap/index.blade.php
+++ resources/views/sitemap/index.blade.php
@@ -1,4 +1,35 @@
-<?xml version="1.0" encoding="UTF-8"?>
+@php
+    echo '<?xml version="1.0" encoding="UTF-8"?>';
+@endphp
+
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+
+    {{-- =====================================================
+         MAIN / STATIC URLS
+    ====================================================== --}}
+
+    @foreach($urls as $url)
+
+        <url>
+            <loc>{{ $url }}</loc>
+        </url>
+
+    @endforeach
+
+
+    {{-- =====================================================
+         CATEGORY URLS
+    ====================================================== --}}
+
+    @foreach($categories as $category)
+
+        <url>
+            <loc>{{ url('/category/' . $category->slug) }}</loc>
+        </url>
+
+    @endforeach
+
+</urlset>
 
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 
