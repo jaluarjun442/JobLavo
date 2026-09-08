599ed16f45ea9487b88cc0273a8553dda6b6b75c
Arjun <jaluarjun442@gmail.com>
2026-09-08 07:34:59 +0530

    adsterra added

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -453,8 +453,18 @@
                     </div>
 
 
-
-
+                <div class="text-center">
+                    <script>
+                    atOptions = {
+                        'key' : '3fcb7650575fb10437b0505607d5653d',
+                        'format' : 'iframe',
+                        'height' : 50,
+                        'width' : 320,
+                        'params' : {}
+                    };
+                    </script>
+                    <script src="https://www.highrevenueformat.com/3fcb7650575fb10437b0505607d5653d/invoke.js"></script>
+                </div>
 
                 @if($post->featured_image)
 
@@ -526,9 +536,31 @@
                         </div>
 
                     @endif
+<div class="text-center">
 
 
+                    <script>
+                    atOptions = {
+                        'key' : 'edce0a4519a4dc72fb023ee610868ada',
+                        'format' : 'iframe',
+                        'height' : 250,
+                        'width' : 300,
+                        'params' : {}
+                    };
+                    </script>
+                    <script src="https://www.highrevenueformat.com/edce0a4519a4dc72fb023ee610868ada/invoke.js"></script>
+                    <script>
+                    atOptions = {
+                        'key' : 'edce0a4519a4dc72fb023ee610868ada',
+                        'format' : 'iframe',
+                        'height' : 250,
+                        'width' : 300,
+                        'params' : {}
+                    };
+                    </script>
+                    <script src="https://www.highrevenueformat.com/edce0a4519a4dc72fb023ee610868ada/invoke.js"></script>
 
+</div>
                     @if($post->content)
 
                         <div class="p-3 p-md-4">
@@ -543,9 +575,10 @@
 
                     @endif
 
-
-
-                  
+                <div class="text-center">
+                    <script async="async" data-cfasync="false" src="https://pl31237402.profitableratecpmnetwork.com/f3a8bce8b6affdbb2b4ca6fd7bfb365d/invoke.js"></script>
+                    <div id="container-f3a8bce8b6affdbb2b4ca6fd7bfb365d"></div>
+                </div>
 
                 </article>
 
